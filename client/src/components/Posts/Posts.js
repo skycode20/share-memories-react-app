161d2decab6bcc163dc9668e60263e5ef0644a90
Skyler R <skyler.rencher@gmail.com>
2021-06-04 00:12:56 -0400

    added a circular progress component if no posts and added a grid structure for the posts to show up. then created the actual structure of the posts and how they'll show up on the ui.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from "react-redux";
+
 import Post from './Post/Post'
 import useStyles from './styles';
 
@@ -7,15 +9,19 @@ const Posts = () => {
     const posts = useSelector((state) => state.posts );
     const classes = useStyles();
 
-    console.log(posts);
+    // console.log(posts);
 
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
-    )
-}
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3} >
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
+    );
+};
 
 export default Posts;
